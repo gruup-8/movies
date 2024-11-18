9b48974ab374b9b5bd18232c17551dc3fff30741
Nilssoni <martikainen93@gmail.com>
2024-11-18 21:06:29 +0200

    added function to get movie poster

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,5 @@
 const BASE_URL = 'https://api.themoviedb.org/3';
+const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
 
 /* 
 ////////////////////\\\\\\\\\\\\\\\\\\\\
@@ -14,6 +15,17 @@ const headers = {
   'Authorization': `Bearer ${API_TOKEN}`,
   'Content-Type': 'application/json',
 };
+
+// Movie poster
+const transformMovieData = (movies) =>
+  movies.map((movie) => ({
+    id: movie.id,
+    title: movie.title,
+    image: movie.poster_path ? `${IMAGE_BASE_URL}${movie.poster_path}` : null,
+    overview: movie.overview,
+    release_date: movie.release_date,
+  }));
+
 
 // Fetch genres from TMDb API
 export const fetchGenres = async () => {
@@ -38,11 +50,22 @@ export const fetchGenres = async () => {
 // Fetch movies by name
 export const fetchMoviesByName = async (query, page) => {
   const response = await fetch(
-    `${BASE_URL}/search/movie?query=${query}&language=en-US&page=${page}`,
+    `${BASE_URL}/search/movie?query=${query}&include_adult=false&language=en-US&page=${page}`,
     { headers }
   );
+
+  if(!response.ok){
+    throw new Error(`Error fetching movies: ${response.statusText}`);
+  }
+
   const data = await response.json();
-  return data;
+
+  console.log('Fetched Movies:', data.results);
+
+  return {
+    results: transformMovieData(data.results),
+    total_pages: data.total_pages,
+  };
 };
 
 // Fetch movies by genre
@@ -51,8 +74,17 @@ export const fetchMoviesByGenre = async (genreId, page) => {
     `${BASE_URL}/discover/movie?with_genres=${genreId}&language=en-US&page=${page}`,
     { headers }
   );
+
+  if(!response.ok) {
+    throw new Error(`Error fetching movies: ${response.statusText}`);
+  }
   const data = await response.json();
-  return data;
+
+  // Map through the results and format them to include the image
+  return {
+    results: transformMovieData(data.results),
+    total_pages: data.total_pages,
+  };
 };
 
 // Fetch top-rated movies
@@ -61,6 +93,8 @@ export const fetchTopRatedMovies = async (page) => {
     headers,
   });
   const data = await response.json();
-  return data;
+  return {
+    results: transformMovieData(data.results),
+    total_pages: data.total_pages,
+  };
 };
-
